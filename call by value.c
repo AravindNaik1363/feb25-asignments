d534d9a7eae423d13440f42e5464a5f9763217ab
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-25 20:25:37 +0530

    call by value in pointers

---
+++ call by value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,b=20;
+	printf("Before swap:\t %d \t %d\n",a,b);
+	swap(a,b);
+}
+swap(int x,int y)
+{
+	int temp=x;
+	x=y;
+	y=temp;
+	printf("After swap:\t %d \t %d",x,y);
+}
